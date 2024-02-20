e5ab37972de606613945132025a3e6737cc09f98
Georgi Terziyski <georgi.terziyski@hpe.com>
2024-02-21 01:25:39 +0200

    Refactor code to improve performance and readability

--- model_kubeflow.py
+++ model_kubeflow.py
@@ -1,39 +1,25 @@
 import kfp
-import kfp.components as comp
-import requests
 import kfp.dsl as dsl
 
-def manage_outliers(df, column, method='IQR'):
-    """
-    Manage outliers in a dataframe column.
-
-    Parameters:
-    df (DataFrame): The Pandas DataFrame.
-    column (str): The column name where to look for and manage outliers.
-    method (str): Method to use for managing outliers. Default is 'IQR' (Interquartile Range).
-
-    Returns:
-    DataFrame: DataFrame with outliers managed.
-    """
-    df = df.copy()
-
-    # Interquartile Range Method
-    if method == 'IQR':
-        Q1 = df[column].quantile(0.25)
-        Q3 = df[column].quantile(0.75)
-        IQR = Q3 - Q1
-        lower_bound = Q1 - 1.5 * IQR
-        upper_bound = Q3 + 1.5 * IQR
-
-        df[column] = df[column].clip(lower=lower_bound, upper=upper_bound)
-
-    return df
-
 def prepare_data():
+    def manage_outliers(df, column, method='IQR'):
+        df = df.copy()
+
+        # Interquartile Range Method
+        if method == 'IQR':
+            Q1 = df[column].quantile(0.25)
+            Q3 = df[column].quantile(0.75)
+            IQR = Q3 - Q1
+            lower_bound = Q1 - 1.5 * IQR
+            upper_bound = Q3 + 1.5 * IQR
+
+            df[column] = df[column].clip(lower=lower_bound, upper=upper_bound)
+
+        return df
     import pandas as pd
     print("---- Inside prepare_data component ----")
     # Load dataset
-    house_data = pd.read_csv("house.csv")
+    house_data = pd.read_csv("https://raw.githubusercontent.com/MaverickGT/House/main/house.csv")
     house_data = house_data.dropna()
     # List of numerical columns to check for outliers
     numerical_columns = ['SqFt', 'Bedrooms', 'Bathrooms', 'Offers', 'Price']
@@ -44,7 +30,7 @@ def prepare_data():
         
     house_data = pd.get_dummies(house_data)
     house_data.to_csv(f'data/final_house.csv', index=False)
-    print("\n ---- data csv is saved to PV location final_house.csv ----")
+    print("\n ---- data csv is saved to final_house.csv ----")
 
 def train_test_split():
     import pandas as pd
@@ -56,7 +42,7 @@ def train_test_split():
     X = final_data.loc[:, final_data.columns != target_column]
     y = final_data.loc[:, final_data.columns == target_column]
     
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1,stratify = y, random_state=40)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=40)
     
     np.save(f'data/X_train.npy', X_train)
     np.save(f'data/X_test.npy', X_test)
@@ -79,12 +65,11 @@ def train_test_split():
     print("\n")
     print(y_test)
 
-def training_basic_classifier():
-    import pandas as pd
+def training_price():
     import numpy as np
     from sklearn.linear_model import LinearRegression
     
-    print("---- Inside training_basic_classifier component ----")
+    print("---- Inside training_price component ----")
     
     X_train = np.load(f'data/X_train.npy',allow_pickle=True)
     y_train = np.load(f'data/y_train.npy',allow_pickle=True)
@@ -95,10 +80,9 @@ def training_basic_classifier():
     with open(f'data/model.pkl', 'wb') as f:
         pickle.dump(classifier, f)
     
-    print("\n linear regression classifier is trained on house data and saved to location /data/model.pkl ----")
+    print("\n Linear regression is trained on house data and saved to location /data/model.pkl ----")
 
 def predict_on_test_data():
-    import pandas as pd
     import numpy as np
     import pickle
     print("---- Inside predict_on_test_data component ----")
@@ -108,44 +92,20 @@ def predict_on_test_data():
     y_pred = linear_reg_model.predict(X_test)
     np.save(f'data/y_pred.npy', y_pred)
     
-    print("\n---- Predicted classes ----")
+    print("\n---- Predicted prices ----")
     print("\n")
     print(y_pred)
 
-def predict_prob_on_test_data():
-    import pandas as pd
-    import numpy as np
-    import pickle
-    print("---- Inside predict_prob_on_test_data component ----")
-    with open(f'data/model.pkl','rb') as f:
-        linear_reg_model = pickle.load(f)
-    X_test = np.load(f'data/X_test.npy',allow_pickle=True)
-    y_pred_prob = linear_reg_model.predict_proba(X_test)
-    np.save(f'data/y_pred_prob.npy', y_pred_prob)
-    
-    print("\n---- Predicted Probabilities ----")
-    print("\n")
-    print(y_pred_prob)
-
 def get_metrics():
-    import pandas as pd
     import numpy as np
-    from sklearn.metrics import accuracy_score,precision_score,recall_score,log_loss
-    from sklearn import metrics
+    from sklearn.metrics import r2_score, mean_squared_error
+    
     print("---- Inside get_metrics component ----")
     y_test = np.load(f'data/y_test.npy',allow_pickle=True)
     y_pred = np.load(f'data/y_pred.npy',allow_pickle=True)
-    y_pred_prob = np.load(f'data/y_pred_prob.npy',allow_pickle=True)
-    acc = accuracy_score(y_test, y_pred)
-    prec = precision_score(y_test, y_pred,average='micro')
-    recall = recall_score(y_test, y_pred,average='micro')
-    entropy = log_loss(y_test, y_pred_prob)
-    
-    y_test = np.load(f'data/y_test.npy',allow_pickle=True)
-    y_pred = np.load(f'data/y_pred.npy',allow_pickle=True)
-    print(metrics.classification_report(y_test, y_pred))
-
-    print("\n Model Metrics:", {'accuracy': round(acc, 2), 'precision': round(prec, 2), 'recall': round(recall, 2), 'entropy': round(entropy, 2)})
+    acc = r2_score(y_test, y_pred)
+    mse = mean_squared_error(y_test, y_pred)
+    print("\n Model Metrics:", {'accuracy': round(acc, 2), 'mse': round(mse, 2)})
 
 create_step_prepare_data = kfp.components.create_component_from_func(
     func=prepare_data,
@@ -159,20 +119,14 @@ create_step_train_test_split = kfp.components.create_component_from_func(
     packages_to_install=['pandas==2.2.0','numpy==1.26.3','scikit-learn==1.4.0']
 )
 
-create_step_training_basic_classifier = kfp.components.create_component_from_func(
-    func=training_basic_classifier,
+create_step_training_price = kfp.components.create_component_from_func(
+    func=training_price,
     base_image='python:3.10',
     packages_to_install=['pandas==2.2.0','numpy==1.26.3','scikit-learn==1.4.0']
 )
 
 create_step_predict_on_test_data = kfp.components.create_component_from_func(
     func=predict_on_test_data,
-    base_image='python:3.10',
-    packages_to_install=['pandas==2.2.0','numpy==1.26.3','scikit-learn==1.4.0']
-)
-
-create_step_predict_prob_on_test_data = kfp.components.create_component_from_func(
-    func=predict_prob_on_test_data,
     base_image='python:3.10',
     packages_to_install=['pandas==2.2.0','numpy==1.26.3','scikit-learn==1.4.0']
 )
@@ -198,17 +152,15 @@ def house_predictor_pipeline(data_path: str):
     
     prepare_data_task = create_step_prepare_data().add_pvolumes({data_path: vop.volume})
     train_test_split = create_step_train_test_split().add_pvolumes({data_path: vop.volume}).after(prepare_data_task)
-    classifier_training = create_step_training_basic_classifier().add_pvolumes({data_path: vop.volume}).after(train_test_split)
-    log_predicted_class = create_step_predict_on_test_data().add_pvolumes({data_path: vop.volume}).after(classifier_training)
-    log_predicted_probabilities = create_step_predict_prob_on_test_data().add_pvolumes({data_path: vop.volume}).after(log_predicted_class)
-    log_metrics_task = create_step_get_metrics().add_pvolumes({data_path: vop.volume}).after(log_predicted_probabilities)
+    price_training = create_step_training_price().add_pvolumes({data_path: vop.volume}).after(train_test_split)
+    log_predicted_price = create_step_predict_on_test_data().add_pvolumes({data_path: vop.volume}).after(price_training)
+    log_metrics_task = create_step_get_metrics().add_pvolumes({data_path: vop.volume}).after(log_predicted_price)
 
     
     prepare_data_task.execution_options.caching_strategy.max_cache_staleness = "P0D"
     train_test_split.execution_options.caching_strategy.max_cache_staleness = "P0D"
-    classifier_training.execution_options.caching_strategy.max_cache_staleness = "P0D"
-    log_predicted_class.execution_options.caching_strategy.max_cache_staleness = "P0D"
-    log_predicted_probabilities.execution_options.caching_strategy.max_cache_staleness = "P0D"
+    price_training.execution_options.caching_strategy.max_cache_staleness = "P0D"
+    log_predicted_price.execution_options.caching_strategy.max_cache_staleness = "P0D"
     log_metrics_task.execution_options.caching_strategy.max_cache_staleness = "P0D"
 
 kfp.compiler.Compiler().compile(
@@ -217,7 +169,7 @@ kfp.compiler.Compiler().compile(
 
 client = kfp.Client()
 
-DATA_PATH = '/data'
+DATA_PATH = './data'
 
 import datetime
 print(datetime.datetime.now().date())
